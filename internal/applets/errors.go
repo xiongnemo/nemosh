31ccce2334b7c251c6fa1744618e8d958dd909a1
Nemo Xiong <nemo@anzupop.com>
2026-07-08 11:17:09 +0800

    Add applet status errors
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- internal/applets/errors.go
+++ internal/applets/errors.go
@@ -1,5 +1,28 @@
 package applets
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var ErrExitFalse = errors.New("false")
+
+type statusError struct {
+	code int
+}
+
+func (e statusError) Error() string {
+	return fmt.Sprintf("exit status %d", e.code)
+}
+
+func ExitStatus(code int) error {
+	return statusError{code: code}
+}
+
+func StatusCode(err error) (int, bool) {
+	var status statusError
+	if !errors.As(err, &status) {
+		return 0, false
+	}
+	return status.code, true
+}
